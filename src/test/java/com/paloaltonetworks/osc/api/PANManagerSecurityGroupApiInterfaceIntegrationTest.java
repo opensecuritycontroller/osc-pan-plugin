0cf3a816844c3e1cd1c9aaafce074ac42a0bbfce
dmitryintel <dmitry.mx.gerenrot@intel.com>
2018-01-08 14:26:28 -0800

    Fix the removeTag bug. (#16)
    * Fix the removeTag call. Make sure only one tag removed.
    
    * Tested with Panorama 7.1 and 8
    
    * Address review

--- src/test/java/com/paloaltonetworks/osc/api/PANManagerSecurityGroupApiInterfaceIntegrationTest.java
+++ src/test/java/com/paloaltonetworks/osc/api/PANManagerSecurityGroupApiInterfaceIntegrationTest.java
@@ -24,6 +24,7 @@ import com.paloaltonetworks.panorama.api.mapping.AddressEntry;
 
 public class PANManagerSecurityGroupApiInterfaceIntegrationTest
     extends AbstractPANApiIntegrationTest {
+    private static final String TESTIP_1 = "1.1.1.1";
     private static final String SG_ID = "123454321";
     private static final String SGNAME = "123454321";
     private static final String SG_INTERFACE_ID = "6789876";
@@ -38,7 +39,10 @@ public class PANManagerSecurityGroupApiInterfaceIntegrationTest
     private SecurityGroupInterfaceElement sgInterfaceElement;
 
     @Mock
-    private ManagerPolicyElement existingPolicyTag;
+    private ManagerPolicyElement existingPolicyTag1;
+
+    @Mock
+    private ManagerPolicyElement existingPolicyTag2;
 
     private PANManagerSecurityGroupApi sgApi;
     private PANManagerSecurityGroupInterfaceApi sgInterfaceApi;
@@ -55,15 +59,17 @@ public class PANManagerSecurityGroupApiInterfaceIntegrationTest
         MockitoAnnotations.initMocks(this);
         // OSC_SecurityGroup_
 
-        Mockito.when(this.sgMember1.getIpAddresses()).thenReturn(asList("1.1.1.1"));
-        Mockito.when(this.sgMember1.getMacAddresses()).thenReturn(asList("1.1.1.1"));
+        Mockito.when(this.sgMember1.getIpAddresses()).thenReturn(asList(TESTIP_1));
+        Mockito.when(this.sgMember1.getMacAddresses()).thenReturn(asList(TESTIP_1));
 
         Mockito.when(this.with1.getMembers()).thenReturn(asList(this.sgMember1));
 
-        Mockito.when(this.existingPolicyTag.getName()).thenReturn(EXISTING_POLICY_TAG);
+        Mockito.when(this.existingPolicyTag1.getName()).thenReturn(EXISTING_POLICY_TAG);
+        Mockito.when(this.existingPolicyTag2.getName()).thenReturn(EXISTING_POLICY_TAG_OTHER);
 
         Set<ManagerPolicyElement> policyElements = new HashSet<>();
-        policyElements.add(this.existingPolicyTag);
+        policyElements.add(this.existingPolicyTag1);
+        policyElements.add(this.existingPolicyTag2);
 
         Mockito.when(this.sgInterfaceElement.getManagerSecurityGroupInterfaceId()).thenReturn(SG_INTERFACE_ID);
         Mockito.when(this.sgInterfaceElement.getManagerSecurityGroupId()).thenReturn("OSCSecurityGroup_" + SG_ID);
@@ -81,6 +87,7 @@ public class PANManagerSecurityGroupApiInterfaceIntegrationTest
     @Ignore
     @Test
     public void testCRUDOperations() throws Exception {
+
         List<AddressEntry> addresses =  this.panClient.fetchAddressesWithTag(EXISTING_POLICY_TAG, this.devGroup);
         assertEquals(0, addresses.size());
 
@@ -88,17 +95,56 @@ public class PANManagerSecurityGroupApiInterfaceIntegrationTest
         addresses =  this.panClient.fetchAddressesWithTag(EXISTING_POLICY_TAG, this.devGroup);
         assertEquals(0, addresses.size());
 
+        addresses =  this.panClient.fetchAddressesWithTag(EXISTING_POLICY_TAG_OTHER, this.devGroup);
+        assertEquals(0, addresses.size());
+
+        addresses =  this.panClient.fetchAddressesWithTag(this.sgTag, this.devGroup);
+        assertEquals(1, addresses.size());
+        assertEquals(addresses.get(0).getName(), TESTIP_1);
+
+        // Act 1
         String sgInterfaceId = this.sgInterfaceApi.createSecurityGroupInterface(this.sgInterfaceElement);
         assertEquals(this.sgTag + "_" + this.devGroup, sgInterfaceId);
 
+        // Assert 1
         addresses =  this.panClient.fetchAddressesWithTag(EXISTING_POLICY_TAG, this.devGroup);
         assertEquals(1, addresses.size());
-        assertEquals(addresses.get(0).getName(), "1.1.1.1");
+        assertEquals(addresses.get(0).getName(), TESTIP_1);
 
-        this.sgInterfaceApi.deleteSecurityGroupInterface(sgInterfaceId);
+        addresses =  this.panClient.fetchAddressesWithTag(EXISTING_POLICY_TAG_OTHER, this.devGroup);
+        assertEquals(1, addresses.size());
+        assertEquals(addresses.get(0).getName(), TESTIP_1);
+
+        addresses =  this.panClient.fetchAddressesWithTag(this.sgTag, this.devGroup);
+        assertEquals(1, addresses.size());
+        assertEquals(addresses.get(0).getName(), TESTIP_1);
+
+        // Act 2
+        this.panClient.removeTagFromAddress(TESTIP_1, EXISTING_POLICY_TAG, this.devGroup);
+
+        // Assert 2
         addresses =  this.panClient.fetchAddressesWithTag(EXISTING_POLICY_TAG, this.devGroup);
         assertEquals(0, addresses.size());
 
+        addresses =  this.panClient.fetchAddressesWithTag(EXISTING_POLICY_TAG_OTHER, this.devGroup);
+        assertEquals(1, addresses.size());
+
+        // Act 3
+        this.sgInterfaceApi.deleteSecurityGroupInterface(sgInterfaceId);
+
+        // Assert 3
+        addresses =  this.panClient.fetchAddressesWithTag(EXISTING_POLICY_TAG, this.devGroup);
+        assertEquals(0, addresses.size());
+
+        addresses =  this.panClient.fetchAddressesWithTag(EXISTING_POLICY_TAG_OTHER, this.devGroup);
+        assertEquals(0, addresses.size());
+
+        addresses =  this.panClient.fetchAddressesWithTag(this.sgTag, this.devGroup);
+        assertEquals(1, addresses.size());
+        assertEquals(addresses.get(0).getName(), TESTIP_1);
+
         this.sgApi.deleteSecurityGroup(this.sgTag);
+        addresses =  this.panClient.fetchAddressesWithTag(this.sgTag, this.devGroup);
+        assertEquals(0, addresses.size());
     }
 }
